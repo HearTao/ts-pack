70bb2f931bd0386bb77e602a1acc357f684c1d1f
Wenlu Wang <wenlwan@microsoft.com>
2020-10-22 13:42:43 +0800

    Add output result file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import { pack } from '.'
+import * as fs from 'fs'
 
-console.log(pack('./tests/a.ts'))
+const result = pack('./tests/a.ts')
+console.log(result)
+fs.writeFileSync('./out.js', result, { encoding: 'utf-8' })
